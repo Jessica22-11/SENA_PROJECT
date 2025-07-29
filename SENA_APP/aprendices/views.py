f118fcf9e56fbeecd3fd19a55e9d27dd270502ed
Jessica Alfonso <taticalinda221@gmail.com>
2025-07-29 08:33:21 -0500

    Se inicializo el proyecto de SENA_APP

---
+++ SENA_APP/aprendices/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from .models import Aprendiz
+from django.template import loader
+
+def aprendices(request):
+    lista_aprendices = Aprendiz.objects.all().order_by('nombre', 'apellido')
+    template = loader.get_template('lista_aprendices.html')
+    context = {
+        'lista-aprendices' :lista_aprendices,
+        'total_aprendices': lista_aprendices.count(),
+    }
+    return HttpResponse(template.render(context, request))
+
+def  inicio(request):
+    template = loader.get_template('inicio.html')
+    return HttpResponse(template.render())
